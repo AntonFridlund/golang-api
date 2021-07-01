ef9c3afd4e1186fb80f5ea8c7b277bfb0e55372a
AntonFridlund <anton.fridlund@mail.com>
2021-07-01 20:59:19 +0200

    Add new user controller

--- controllers/user/user_controller.go
+++ controllers/user/user_controller.go
@@ -2,6 +2,10 @@ package user
 
 import "github.com/gofiber/fiber/v2"
 
+func User(c *fiber.Ctx) error {
+	return c.SendString("You are now at: \"/user\"\nHere you could display some user information.")
+}
+
 func NewUser(c *fiber.Ctx) error {
-	return c.SendString("YOU MADE IT! We are now at: \"/user/new\" 🎉")
+	return c.SendString("You are now at: \"/user/new\".\nHere you could create a new user.")
 }
