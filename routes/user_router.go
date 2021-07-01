333059e780f2111cac012835bbef92a99ed1cf25
AntonFridlund <anton.fridlund@mail.com>
2021-07-01 20:58:48 +0200

    Add base route

--- routes/user_router.go
+++ routes/user_router.go
@@ -8,6 +8,7 @@ import (
 
 func User() *fiber.App {
 	router := fiber.New()                  // Register a new router.
+	router.Get("/", controller.User)       // Register a new route for GET in /user.
 	router.Get("/new", controller.NewUser) // Register a new route for GET in /user.
 	return router                          // Return the router in order to mount it.
 }
