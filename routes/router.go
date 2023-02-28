7a692e9f93c8794b471b13e4440301744d63a738
prebox <anton.fridlund@mail.com>
2023-02-28 15:20:46 +0100

    Add main router

--- routes/router.go
+++ routes/router.go
@@ -1,7 +1,20 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	api "main/routes/api/v1"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+// Sets up sub-routes.
 func Setup(app *fiber.App) {
-	app.Mount("/user", User()) // Mount the /user sub-router.
+	app.Route("/api/v1", api.Router)
+
+	// Default api version.
+	app.Route("/api", api.Router)
+
+	// 404 routes.
+	app.Use("*", func(c *fiber.Ctx) error {
+		return fiber.ErrNotFound
+	})
 }
